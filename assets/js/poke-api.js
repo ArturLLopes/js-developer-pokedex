fbaad3e204bcdbbe84373a3f93c711370fbf4cb0
ArturLLopes <arturllopes@gmail.com>
2024-11-09 10:14:12 -0300

    Add das devidas modificações para o desafio

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,6 +1,8 @@
 
 const pokeApi = {}
 
+
+
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
@@ -12,7 +14,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    pokemon.photo = pokeDetail.sprites.front_shiny
 
     return pokemon
 }
@@ -21,9 +23,12 @@ pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
+
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 9) => {
+
+
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
@@ -32,4 +37,21 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
-}
+};
+
+
+const getPokemonsDetailed = async (pokemon_name) => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon_name}`;
+
+    const data = await fetch(URL);
+    const response = await data.json();
+    return response;
+};
+
+pokeApi.getPokemonDetailById = (pokemonId) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon);
+};
